6d8bfac55a8d416340ba8c44579e9cd86b4e0efb
Joshua Ongtawco <comrademudkipz@live.com>
2016-04-24 19:23:54 -0700

    Completed AgeMessage3 program

---
+++ GUIs/AgeMessages3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class AgeMessages3
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        String name;
+        int age;
+
+        System.out.print("Your name: ");
+        name = keyboard.next();
+        System.out.print("Your age: ");
+        age = keyboard.nextInt();
+        System.out.println();
+
+        if ( age < 16 )
+        {
+            System.out.println("You can't drive, " + name);
+        }
+
+        if ( age >= 16 && age <= 17 )
+        {
+            System.out.println("You can drive but not vote, " + name);
+        }
+        if ( age >= 18 && age <= 24 )
+        {
+            System.out.println("You can vote but not rent a car, " + name);
+        }
+
+        if ( age >= 25 )
+        {
+         System.out.println("You can do pretty much anything, " + name);
+        }
+    }
+}
